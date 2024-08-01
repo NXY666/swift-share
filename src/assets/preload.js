32bbb6bf5b38177bb6571d30219d283bc099ad33
NXY <62371554+nxy666@users.noreply.github.com>
2024-08-01 11:29:06 +0800

    新增ctrl+v上传支持

--- src/assets/preload.js
+++ src/assets/preload.js
@@ -51,8 +51,7 @@ if ('serviceWorker' in navigator) {
 				}
 
 				const dialog = new SelectUploadDialog(shareFiles, (files) => {
-					uploadFiles(files)
-					.catch(({message}) => alert(message));
+					uploadFiles(files).catch(({message}) => showAlertDialog('上传失败', message));
 				});
 				dialog.open();
 			} else if (typeof shareTargets === "number") {
@@ -69,6 +68,42 @@ if ('serviceWorker' in navigator) {
 					api.post("/upload/text", text)
 					.then(({data}) => showAlertDialog('上传成功', `请凭【${data.code.toUpperCase()}】提取文本。`))
 					.catch(({message}) => showAlertDialog('上传失败', message));
+				});
+				dialog.open();
+			}
+		}
+	});
+}
+
+// 如果可以访问剪贴板api，那么支持Ctrl+V粘贴上传
+if (navigator.clipboard) {
+	document.addEventListener('paste', async () => {
+		// 获取当前焦点元素
+		const activeElement = document.activeElement;
+
+		// 判断当前元素是否是输入框或文本区域等可输入的元素
+		const isInputElement = activeElement.tagName === 'INPUT' || activeElement.tagName === 'TEXTAREA' || activeElement.isContentEditable;
+
+		const {showAlertDialog, ConfirmUploadTextDialog, SelectUploadDialog} = await import("./js/dialog.js");
+
+		if (!isInputElement || activeElement.readOnly || activeElement.disabled) {
+			const clipboardItems = await navigator.clipboard.read();
+			const clipboardItem = clipboardItems[0];
+
+			if (clipboardItem.types.includes('text/plain')) {
+				const textBlob = await clipboardItem.getType('text/plain');
+				const text = await textBlob.text();
+				const dialog = new ConfirmUploadTextDialog(text, () => {
+					api.post("/upload/text", textBlob)
+					.then(({data}) => showAlertDialog('上传成功', `请凭【${data.code.toUpperCase()}】提取文本。`))
+					.catch(({message}) => showAlertDialog('上传失败', message));
+				});
+				dialog.open();
+			} else if (clipboardItem.types.includes('image/png')) {
+				const blob = await clipboardItem.getType('image/png');
+				const file = new File([blob], 'image.png', {type: 'image/png'});
+				const dialog = new SelectUploadDialog([file], (files) => {
+					uploadFiles(files).catch(({message}) => showAlertDialog('上传失败', message));
 				});
 				dialog.open();
 			}
